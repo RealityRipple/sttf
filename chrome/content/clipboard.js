30a6c7354ccf62cba70313d76a08173ef234672a
Andy <webmaster@RealityRipple.com>
2021-03-10 02:48:52 -0800

    Full Length Component Notation

--- chrome/content/clipboard.js
+++ chrome/content/clipboard.js
@@ -2,8 +2,8 @@ function copySelectedTextURL() {
  var currentURL = gBrowser.contentWindow.location.href;
  var hashLoc = currentURL.indexOf("#");
  if (hashLoc > 0) {
-  Cc["@mozilla.org/widget/clipboardhelper;1"].getService(Ci.nsIClipboardHelper).copyString(currentURL.substring(0, hashLoc) + "#:~:text=" + gBrowser.contentWindow.getSelection().toString());
+  Components.classes["@mozilla.org/widget/clipboardhelper;1"].getService(Components.interfaces.nsIClipboardHelper).copyString(currentURL.substring(0, hashLoc) + "#:~:text=" + gBrowser.contentWindow.getSelection().toString());
  } else {
-  Cc["@mozilla.org/widget/clipboardhelper;1"].getService(Ci.nsIClipboardHelper).copyString(currentURL + "#:~:text=" + gBrowser.contentWindow.getSelection().toString());
+  Components.classes["@mozilla.org/widget/clipboardhelper;1"].getService(Components.interfaces.nsIClipboardHelper).copyString(currentURL + "#:~:text=" + gBrowser.contentWindow.getSelection().toString());
  }
 }
