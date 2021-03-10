8e66258bdf2c1a45eeeea3541ce158d00b7cca0a
Andy <webmaster@RealityRipple.com>
2021-03-10 11:19:20 -0800

    Bundle Clipboard Functions

--- chrome/content/clipboard.js
+++ chrome/content/clipboard.js
@@ -1,12 +1,14 @@
-function copySelectedTextURL()
-{
- let cWindow = gBrowser.contentWindow
- let currentURL = cWindow.location.href;
- let hashLoc = currentURL.indexOf("#");
- let clipboardCopy = Components.classes["@mozilla.org/widget/clipboardhelper;1"].getService(Components.interfaces.nsIClipboardHelper).copyString
- let appendToURL = "#:~:text=" + cWindow.getSelection().toString()
- if (hashLoc > 0)
-  clipboardCopy(currentURL.substring(0, hashLoc) + appendToURL);
- else
-  clipboardCopy(currentURL + appendToURL);
+var sttf_clipboard = {
+ copySelectedTextURL: function()
+ {
+  let cWindow = gBrowser.contentWindow
+  let currentURL = cWindow.location.href;
+  let hashLoc = currentURL.indexOf("#");
+  let clipboardCopy = Components.classes["@mozilla.org/widget/clipboardhelper;1"].getService(Components.interfaces.nsIClipboardHelper).copyString
+  let appendToURL = "#:~:text=" + cWindow.getSelection().toString()
+  if (hashLoc > 0)
+   clipboardCopy(currentURL.substring(0, hashLoc) + appendToURL);
+  else
+   clipboardCopy(currentURL + appendToURL);
+ }
 }
