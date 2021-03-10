b97c3ad8b9815a851f87596c230f0dac785657c3
Andy <webmaster@RealityRipple.com>
2021-03-10 11:01:20 -0800

    Standardize Braces Use

--- chrome/content/clipboard.js
+++ chrome/content/clipboard.js
@@ -1,12 +1,12 @@
-function copySelectedTextURL() {
- var cWindow = gBrowser.contentWindow
- var currentURL = cWindow.location.href;
- var hashLoc = currentURL.indexOf("#");
- var clipboardCopy = Components.classes["@mozilla.org/widget/clipboardhelper;1"].getService(Components.interfaces.nsIClipboardHelper).copyString
- var appendToURL = "#:~:text=" + cWindow.getSelection().toString()
- if (hashLoc > 0) {
+function copySelectedTextURL()
+{
+ let cWindow = gBrowser.contentWindow
+ let currentURL = cWindow.location.href;
+ let hashLoc = currentURL.indexOf("#");
+ let clipboardCopy = Components.classes["@mozilla.org/widget/clipboardhelper;1"].getService(Components.interfaces.nsIClipboardHelper).copyString
+ let appendToURL = "#:~:text=" + cWindow.getSelection().toString()
+ if (hashLoc > 0)
   clipboardCopy(currentURL.substring(0, hashLoc) + appendToURL);
- } else {
+ else
   clipboardCopy(currentURL + appendToURL);
- }
 }
