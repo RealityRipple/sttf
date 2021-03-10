69bfa83efc459f26e666f72b380b613fb7df1899
Job Bautista <jobbautista9@protonmail.com>
2021-03-10 17:54:28 +0800

    Account for cases when current URL contains a hash

--- chrome/content/clipboard.js
+++ chrome/content/clipboard.js
@@ -1,3 +1,9 @@
 function copySelectedTextURL() {
- Cc["@mozilla.org/widget/clipboardhelper;1"].getService(Ci.nsIClipboardHelper).copyString(gBrowser.currentURI.spec + "#:~:text=" + gBrowser.contentWindow.getSelection().toString());
+ var currentURL = gBrowser.contentWindow.location.href;
+ var hashLoc = currentURL.indexOf("#");
+ if (hashLoc > 0) {
+  Cc["@mozilla.org/widget/clipboardhelper;1"].getService(Ci.nsIClipboardHelper).copyString(currentURL.substring(0, hashLoc) + "#:~:text=" + gBrowser.contentWindow.getSelection().toString());
+ } else {
+  Cc["@mozilla.org/widget/clipboardhelper;1"].getService(Ci.nsIClipboardHelper).copyString(currentURL + "#:~:text=" + gBrowser.contentWindow.getSelection().toString());
+ }
 }
